40105fd404ac841037c6c527169ade84277a2bb4
Shubham Tiwari <websbiz0000@gmail.com>
2018-11-11 08:31:47 +0000

    adds features

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+ 
+module.exports = {
+    mode: 'production',
+    entry: './index.js',
+    output: {
+        path: path.resolve('./'),
+        filename: 'build.js',
+        libraryTarget: 'commonjs2'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules)/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
